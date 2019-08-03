ffa1498c9d8d022c59b5fde26e94c6d994f58453
Liam Dalling <git@liamdalling.co.uk>
2019-08-03 10:07:36 +0100

    Added base for developing case study problem

---
+++ CaseStudyCard/Data/CaseStudyCardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace CaseStudyCard.Models
+{
+    public class CaseStudyCardContext : DbContext
+    {
+        public CaseStudyCardContext (DbContextOptions<CaseStudyCardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ CaseStudyCard/Models/ApplicationForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseStudyCard.Models
+{
+    public class ApplicationForm
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private DateTime DOB { get; set; }
+        private string Last { get; set; }
+
+    }
+}
